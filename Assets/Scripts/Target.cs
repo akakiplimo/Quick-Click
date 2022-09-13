dde5cdaf35a945666a8700923eb3378ec848507f
A. Kiplimo <akakiplimo@gmail.com>
2022-09-13 20:54:00 +0300

    Destroy Targets with click and sensor trigger

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -9,7 +9,7 @@ public class Target : MonoBehaviour
     private float maxSpeed = 16;
     private float torque = 10;
     private float xRangePos = 4;
-    private float ySpawnPos = -6;
+    private float ySpawnPos = -2;
 
     // Start is called before the first frame update
     void Start()
@@ -28,16 +28,31 @@ public class Target : MonoBehaviour
         
     }
 
+    // Destroy Object on Mouse Click like in Fruit Ninja
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    // Destroy Object once it enters the trigger of the Sensor after leaving game view to free resources
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
+    // Force
     Vector3 RandomForce()
     {
         return Vector3.up * Random.Range(minSpeed, maxSpeed);
     }
 
+    // Torque
     float RandomTorque()
     {
         return Random.Range(-torque, torque);
     }
 
+    // Position
     Vector3 RandomSpawnPos()
     {
         return new Vector3(Random.Range(-xRangePos, xRangePos), ySpawnPos);
